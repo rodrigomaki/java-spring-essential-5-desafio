a5676cfc04fa38931070d9cd52728a9ffbe95908
Rodrigo Maki <rodrigomaki@gmail.com>
2023-07-08 17:54:51 -0300

    initial commit - project started

---
+++ src/main/java/com/desafio/componentes/ComponentesApplication.java
@@ -0,0 +1,15 @@
+package com.desafio.componentes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponentesApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ComponentesApplication.class, args);
+
+	}
+
+}
