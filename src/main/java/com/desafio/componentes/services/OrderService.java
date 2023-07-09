581cdf086adc2bce54245635df6fd4ec16fa3f2f
Rodrigo Maki <rodrigomaki@gmail.com>
2023-07-09 20:15:36 -0300

    add OrderService

---
+++ src/main/java/com/desafio/componentes/services/OrderService.java
@@ -0,0 +1,17 @@
+package com.desafio.componentes.services;
+
+import com.desafio.componentes.entities.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private ShippingService shippingService;
+
+    public double total(Order order) {
+        return order.getBasic() * (order.getDiscount() / 100) + shippingService.shipment(order);
+    }
+
+}
